a1e4bfafa33ab8d3a49163c2c3b25bde85aad257
Draxiio <33795015+Draxiio@users.noreply.github.com>
2018-02-07 17:55:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ bot.on('ready', () => {
     console.log("ElityBot Ready");
 });
 
-bot.login('Mzg5MTMyODE1MDQxNTYwNTg2.DVy-kA.BWzwgzK091UpWPuYlPT-TnBsX2U');
+bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
