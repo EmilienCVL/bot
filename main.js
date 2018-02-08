00891d7d8fa064539846833f818402abc7f7c398
Draxiio <33795015+Draxiio@users.noreply.github.com>
2018-02-08 18:26:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ bot.on('ready', () => {
     console.log("ElityBot Ready");
 });
 
-bot.login(process.env.TOKEN);
+bot.login(process.env.TOKE);
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
         var help_embed = new Discord.RichEmbed()
             .setTitle("ElityBot | Commandes")
-            .setDescripton("[►](1) !help » Pour afficher les commandes." + "\n[►](2) !ip » Adresse du serveur Minecraft." + "\n[►](3) !mumble » Adresse du serveur Mumble." + "\n[►](4) !boutique » Boutique du serveur." + "\n[►](5) !twitter » Le Twitter d'ElityGames." + "\n[►](6) !staff » Le Staff d'ElitGames.")
+            .setDescription("[►](1) !help » Pour afficher les commandes." + "\n[►](2) !ip » Adresse du serveur Minecraft." + "\n[►](3) !mumble » Adresse du serveur Mumble." + "\n[►](4) !boutique » Boutique du serveur." + "\n[►](5) !twitter » Le Twitter d'ElityGames." + "\n[►](6) !staff » Le Staff d'ElitGames.")
             .setColor('#F49301')
             message.author.sendEmbed(help_embed);
             message.delete();
