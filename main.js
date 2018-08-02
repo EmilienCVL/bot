73d7f1b6681719732e2b6e8798f757f18aab2b6f
EmilienCVL <36304372+EmilienCVL@users.noreply.github.com>
2018-08-02 02:20:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
+import { Webhook } from "discord.js";
+
 const Discord = require("discord.js");
 
 var bot = new Discord.Client();
 var prefix = ("!");
 
 bot.on('ready', () => {
-    bot.user.setPresence({ game: { name: 'ElityGames | !help', type: 0} });
-    console.log("ElityBot ✔");
+    bot.user.setPresence({ game: { name: 'Taupe`Info', type: 0} });
+    console.log("Taupe'Info ✔");
 });
 
 bot.login(process.env.TOKEN);
@@ -16,11 +18,10 @@ bot.on('message', message => {
             .setTitle("ElityBot | Commandes")
             .setDescription("[►](1) !help » Pour afficher les commandes." + "\n[►](2) !ip » Adresse du serveur Minecraft." + "\n[►](3) !mumble » Adresse du serveur Mumble." + "\n[►](4) !boutique » Boutique du serveur." + "\n[►](5) !twitter » Le Twitter d'ElityGames." + "\n[►](6) !staff » Le Staff d'ElitGames.")
             .setColor('#F49301')
-            message.author.send(help_embed);
+            message.author.sendEmbed(help_embed);
             if (message.guild != null){
                 message.delete();
                 }
-        //message.channel.sendMessage("Voici les commandes du Bot »\n §help » Pour afficher les commandes.")
         console.log("Commande Help Demandée.");
     }
 
@@ -29,64 +30,10 @@ bot.on('message', message => {
             .setTitle("ElityBot | Minecraft")
             .setDescription("Adresse » mc.elitygames.fr")
             .setColor('#F49301')
-            message.author.send(minecraft_embed);
+            message.author.sendEmbed(minecraft_embed);
             if (message.guild != null){
                 message.delete();
                 }
-    }
-
-    if (message.content === prefix + "mumble"){
-        var mumble_embed = new Discord.RichEmbed()
-            .setTitle("ElityBot | Mumble")
-            .setDescription("Adresse » mumble.elitygames.fr" + "\nPort » 16300")
-            .setColor('#F49301')
-            message.author.send(mumble_embed);
-            if (message.guild != null){
-                message.delete();
-                }
-    }
-
-    if (message.content === prefix + "boutique"){
-        var boutique_embed = new Discord.RichEmbed()
-            .setTitle("ElityBot | Boutique")
-            .setDescription("Boutique » http://elity.buycraft.net/")
-            .setColor('#F49301')
-            message.author.send(boutique_embed);
-            if (message.guild != null){
-                message.delete();
-                }
-    }
-    
-    if (message.content === prefix + "twitter"){
-        var twitter_embed = new Discord.RichEmbed()
-            .setTitle("ElityBot | Twitter")
-            .setDescription("Twitter » https://twitter.com/ElityGames")
-            .setColor('#F49301')
-            message.author.send(twitter_embed);
-            if (message.guild != null){
-                message.delete();
-                }
-    }
-
-    if (message.content === prefix + "staff"){
-        var staff_embed = new Discord.RichEmbed()
-            .setTitle("ElityBot | Staff")
-            .addField("» Administration «", "\nMalfanax" + "\nKenai_FDO")
-            .addField("» Community Manager «", "\n     Shadoune666")
-            .addField("» Responsables Modération «", "\nBeZaidHache" + "\nIromega")
-            .addField("» Responsable Host «", "\nAlpharys")
-            .addField("» Responsable Build «", "\nbartix2612")
-            .addField("» Modération «", "\nDraxiio_EG" +"\nOsheip" + "\nTorusMC" + "\nSteyzz" + "\nFuzior_Floki" + "\nTobiramaa" + "\nMinato" + "\nFroggyBalboa" + "\nredbu67600" + "\nSkydoo" + "\nZiffard")
-            .addField("» Helper «", "\nAdrossiel_" + "\nGold_oO" + "\nOver_Rex" + "\nYysm")
-            .addField("» Builder «", "\n__0mega" + "\nLaMieDePain" + "\nLeFauxBelgeTroBo" + "\nzoryken")
-            .addField("» Développeur «", "\nPersonne")
-            .addField("» Graphiste «", "\nPersonne")
-            .setColor('#F49301')
-            message.author.send(staff_embed);
-
-            if (message.guild != null){
-            message.delete();
-            }
     }
 
     else if (message.content.startsWith(prefix + "purge")){
@@ -98,10 +45,10 @@ bot.on('message', message => {
             message.author.send(purge_embed);
             return;
         }
-        let draxiio = message.guild.roles.find("name", "Draxiio");
-        let chocapic = message.guild.roles.find("name", "Chocapic");
+        let joueur = message.guild.roles.find("name", "• Joueur");
+        let tech = message.guild.roles.find("name", "• Technicien");
     
-        if (!message.member.roles.has(draxiio.id) && !message.member.roles.has(chocapic.id)){
+        if (!message.member.roles.has(joeur.id) && !message.member.roles.has(tech.id)){
             purge_embed.setTitle("ElityBot | Purge")
             .setDescription("Vous n'avez pas la permission d'effectuer cette commande.")
             .setColor('#F49301')
